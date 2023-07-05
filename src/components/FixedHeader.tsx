09e6f710990b30997ce880ef82c7241c3bab7cdd
David Morales <davidmolv16@gmail.com>
2023-07-05 08:44:04 +0200

    Proyectos

--- src/components/FixedHeader.tsx
+++ src/components/FixedHeader.tsx
@@ -7,7 +7,7 @@ function FixedHeader() {
       <div className='rounded-xl max-w-[1100px] mx-auto w-fit px-4 h-14 flex place-items-center place-content-center bg-white/20 backdrop-blur-md'>
         <div className='flex place-items-center sm:gap-8 gap-4 [&>a]:text-sm sm:[&>a]:text-lg'>
           <Link className='text-white sm:text-white/80 hover:text-white' href={'/'}>Inicio</Link>
-          <Link className='text-white sm:text-white/80 hover:text-white' href={'/proyectos'}>Proyectos</Link>
+          <Link className='text-white sm:text-white/80 hover:text-white' href={'/#proyectos'}>Proyectos</Link>
           <Link className='text-white sm:text-white/80 hover:text-white' href={'/contacto'}>Contacto</Link>
           <Link className='text-white sm:text-white/80 hover:text-white' href={'/curriculum'}>Currículum</Link>
         </div>
