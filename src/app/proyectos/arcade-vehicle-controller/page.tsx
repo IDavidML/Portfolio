658d2a6f20d3bf08f3df031a8452f1a54d3264ab
David Morales <davidmolv16@gmail.com>
2023-09-29 13:58:22 +0200

    Arcade Vehicle Controller

---
+++ src/app/proyectos/arcade-vehicle-controller/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TransporteDelSur() {
+  return (
+    <iframe className='w-full h-screen' src="/projects/arcade-vehicle-controller/documentation.pdf"/>
+  )
+}
+
+export default TransporteDelSur
